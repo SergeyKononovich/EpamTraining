fbf8ad8c650396a61b00fd94ae4bcd8b0b6a87e0
Sergey Kononovich <Sergey.Kononovich.ll@gmail.com>
2015-10-18 18:11:09 +0300

    Refactor: remove unused directives in solution

--- Task1_TaxiStation/CarSpecificationsBase.cs
+++ Task1_TaxiStation/CarSpecificationsBase.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task1_TaxiStation
+﻿namespace Task1_TaxiStation
 {
     public abstract class CarSpecificationsBase : ICarSpecifications
     {
--- Task1_TaxiStation/TaxiStation.cs
+++ Task1_TaxiStation/TaxiStation.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task1_TaxiStation
 {
