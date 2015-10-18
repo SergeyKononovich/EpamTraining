b8420c69a8e615542ef814eb34eea2ce9750289f
Sergey Kononovich <Sergey.Kononovich.ll@gmail.com>
2015-10-18 23:44:18 +0300

    Implemented GasolineCarSpecifications class

---
+++ Task1_TaxiStation/GasolineCarSpecifications.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_TaxiStation
+{
+    public class GasolineCarSpecifications : CarSpecificationsBase
+    {
+        private float _engineCapacity = 999f;
+        private float _fuelConsumptionLp100Km = 0.1f;
+
+        public override EngineType EngineType => EngineType.Gasoline;
+        public float EngineCapacityL
+        {
+            get { return _engineCapacity; }
+            set
+            {
+                if (value < 0.1)
+                    throw new ArgumentOutOfRangeException(nameof(EngineCapacity), value, "Should be > 0.1");
+
+                _engineCapacity = value;
+            }
+        }
+        public float FuelConsumptionLP100Km
+        {
+            get { return _fuelConsumptionLp100Km; }
+            set
+            {
+                if (value < 0.1)
+                    throw new ArgumentOutOfRangeException(nameof(EngineCapacity), value, "Should be > 0.1");
+
+                _fuelConsumptionLp100Km = value;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                   $"Engine capacity: {EngineCapacityL}\n" +
+                   $"Fuel consumption (L/100km): {FuelConsumptionLP100Km}\n";
+        }
+    }
+}
