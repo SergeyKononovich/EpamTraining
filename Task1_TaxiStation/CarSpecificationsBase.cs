6b43134814fac6685250904f285322ffc1438cf9
Sergey Kononovich <Sergey.Kononovich.ll@gmail.com>
2015-10-18 22:11:29 +0300

    Added CarSpecificationsBase and CarBase abstract classes
    To represent a default implementation of ICarSpecifications and ICar.

---
+++ Task1_TaxiStation/CarSpecificationsBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_TaxiStation
+{
+    public abstract class CarSpecificationsBase : ICarSpecifications
+    {
+        public abstract EngineType EngineType { get; }
+        public int MaxSpeedKPH { get; set; }
+        public int NumberOfSeats { get; set; } = 1;
+        public int AverageCostOfKilometerBYB { get; set; }
+    }
+}
